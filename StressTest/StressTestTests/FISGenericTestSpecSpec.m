ff7b1fddb7ad10553090d4c93889920ebf0bb8a4
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-04 11:29:47 -0400

    add test blocks

--- StressTest/StressTestTests/FISGenericTestSpecSpec.m
+++ StressTest/StressTestTests/FISGenericTestSpecSpec.m
@@ -20,20 +20,95 @@ describe(@"FISGenericTestSpec", ^{
 
     });
     
-    beforeEach(^{
-
-    });
-    
-    it(@"", ^{
-
-    });  
-    
-    afterEach(^{
-
-    });
-    
-    afterAll(^{
-
+    describe(@"our generic tests; let's make them pass! Ignore the back-slashes in the below code, those are there so i can still use obj-c syntax in an NSString", ^{
+        describe(@"let's work with the beKindOf matcher!", ^{
+            it(@"is kind of class NSString",^{
+                <#code#>
+            });
+            
+            it(@"is kind of class NSInteger",^{
+                <#code#>
+            });
+            
+            it(@"is kind of class NSArray",^{
+                <#code#>
+            });
+        });
+        
+        describe(@"let's work with the equal matcher!", ^{
+            it(@"test that 4+4 is equal to 8",^{
+                <#code#>
+            });
+            
+            it(@"create an NSString variable called 'name' with the value Chris. test that your variable is equal to @\"Chris\"",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable named 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is equal to @[@\"poptarts\", @\"hotpockets\"]",^{
+                <#code#>
+            });
+        });
+        
+        describe(@"let's work with the notEqual matcher!", ^{
+            it(@"create an NSString variable called 'name' with the value Chris. test that your variable is not equal to the NSString @\"Joe\"",^{
+                <#code#>
+            });
+            
+            it(@"test that 4+4 is not equal to 18",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"celery\", @\"spinach\"]",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"hotpockets\", @\"poptarts\"]",^{
+                <#code#>
+            });
+        });
+        
+        describe(@"let's work with the beTruthy matcher! make tests below using beTruthy instead of the equal matcher.", ^{
+            it(@"test that 4+4 is equal to 8",^{
+                <#code#>
+            });
+            
+            it(@"create an NSString variable called 'name' with the value Chris. test that your variable is equal to @\"Chris\"",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable named 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is equal to @[@\"poptarts\", @\"hotpockets\"]",^{
+                <#code#>
+            });
+        });
+        
+        describe(@"let's work with the beFalsy matcher! make tests below using beFalsy instead of the notEqual matcher", ^{
+            it(@"create an NSString variable called 'name' with the value Chris. test that your variable is not equal to the NSString @\"Joe\"",^{
+                <#code#>
+            });
+            
+            it(@"test that 4+4 is not equal to 18",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"celery\", @\"spinach\"]",^{
+                <#code#>
+            });
+            
+            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"hotpockets\", @\"poptarts\"]",^{
+                <#code#>
+            });           
+        });
+        
+        describe(@"let's finish with some collection-specific matchers, beginsWith and endsWith", ^{
+            it(@"re-create your 'healthFood' array from earlier in the exercise. test that 'healthFood' begins with @\"poptarts\"",^{
+                <#code#>
+            });
+            
+            it(@"re-create your 'healthFood' array from earlier in the exercise. test that 'healthFood' ends with with @\"hotpockets\"",^{
+                <#code#>
+            });
+        });
+        
     });
 });
 
